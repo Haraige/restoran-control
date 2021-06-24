f2a9bcb1e740942c9044b16651569a7872ec1141
Haraige <harahajchuk@gmail.com>
2021-06-24 14:16:32 +0300

    Init project

---
+++ src/test/java/ua/org/code/restoran/RestoranApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.org.code.restoran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
